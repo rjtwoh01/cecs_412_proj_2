248f09de935628a3eb66db918c224e3b4100822e
DESKTOP-0GGS90G\illod <rjtwoh01@louisville.edu>
2017-06-08 10:01:42 -0400

    Comments and increment

--- conversion.c
+++ conversion.c
@@ -11,18 +11,28 @@ uint8_t fahrenheitFinal[20];
 int main(void) {
 	
 	
+	//loop through sram fahrenheit and write to eeprom
 	for (int i = 0; i < 20; i++) {
 		eeprom_write_byte((uint8_t *)i, fahrenheit[i]); //writing to internal EEPROM as an I/O Device via NVM
 	}
-	
-	//eeprom_read_block ((void*) &fahrenheitFinal, (const void*) 0, 20); //read 20 bytes from EEPROM starting from the first address and store in SRAM
-	for (int i = 0; i < 20; i++) {
-		fahrenheitFinal[i] = eeprom_read_byte((uint8_t* )i);
+
+	//incremented by 20 in eeprom memory
+	for(int i = 20; i < 40; i++)
+	{
+		eeprom_write_byte((uint8_t* )i, fahrenheit[i]); //writing to internal EEPROM as an I/O Device via NVM Controller + 20
+		//look at eeprom EEPROM Memory Window, address 0x0000
 	}
 	
+	//eeprom_read_block ((void*) &fahrenheitFinal, (const void*) 0, 20); //read 20 bytes from EEPROM starting from the first address and store in SRAM
+	//loop through eeprom and write to fahrenheit new
+	for (int i = 0; i < 20; i++) {
+		fahrenheitFinal[i] = eeprom_read_byte((uint8_t* )i); //copies over from eeprom into fahrenheitFinal
+	}
+
+	//loop through fahrenheit array and conver to celsius array
 	for (int i = 0; i < 20; i++) {
 		celsiusConverted[i] = (fahrenheitFinal[i] - 32) / 1.8; //convert fahrenheit to celsius
 	}
 
-	while(1);
+	while(1); //keep program running
 }
